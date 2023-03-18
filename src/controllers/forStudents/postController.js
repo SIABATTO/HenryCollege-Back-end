4c9b22dac385e587a13f37661b9495473f54dd71
Jersitto <jersgonz310@outlook.com>
2023-03-18 16:39:46 -0500

    Models modificados todo funcionando

--- src/controllers/forStudents/postController.js
+++ src/controllers/forStudents/postController.js
@@ -2,14 +2,13 @@ const {Students, Course} = require("../../db")
 
 
 const createStudent = async(info) =>{
-    const{firstName,lastName,email,password,phone, id} = info
+    const{firstName,lastName,email,password,phone} = info
     const newStudent = await Students.create({
         firstName,
         lastName,
         email,
         password,
         phone,
-        id
     })
 return newStudent
 }
