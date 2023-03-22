e7ee0ffc936e082eb72419e3b9965e0655fe0fe2
malbet <feellingokay@gmail.com>
2023-03-22 18:45:35 -0500

    Protected routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require("cookie-parser")
 const cors = require("cors")
 const morgan = require("morgan")
 const router = require("./routes/index")
+const { auth } = require('express-oauth2-jwt-bearer');
 
 const app = express()
 app.use(cors())
@@ -11,7 +12,14 @@ app.use(express.urlencoded({ extended: true })) // esto reemplaza la dependencia
 app.use(cookieParser())
 app.use(morgan("dev")) // el dev  para que todo se muestre en consola
 
-app.use("/", router)
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: 'http://localhost:3001/',
+  issuerBaseURL: `https://dev-js4nbs9b.us.auth0.com/`,
+});
+
+app.use("/", checkJwt, router)
 
 // Error catching endware.
 app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
