4052dd0e947e70839e37e4dad7d49878ee47bc16
Andres Siabato <asiabatto05@gmail.com>
2023-03-22 14:28:16 -0500

    fix deploy db

--- src/db.js
+++ src/db.js
@@ -9,7 +9,8 @@ const {
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {  /* usar en local host */
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/HenryCollege`, {  /* usar cuando se haga deploy */
-  logging: false, // set to console.log to see the raw SQL queries
+
+logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 const basename = path.basename(__filename);
