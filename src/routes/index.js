6e4339b6e8c0d66efdd1fce5cc45e9c0bf023aab
malbet <feellingokay@gmail.com>
2023-03-22 23:52:27 -0500

    Desprotegido la ruta de cursos en general

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,11 @@
 const {Router} = require("express")
 const router = Router()
-const {courseRouter} = require("./courseRouter")
+const courseRouter = require("./courseRouter")
 const studentsRouter = require("./studentsRouter")
 const teachersRouter = require("./teachersRouter")
-const checkJwt = require("../handlers/forAuth/protectRoutes")
 
 // Rutas protegidas
-router.use("/course", checkJwt, courseRouter)
+router.use("/course", courseRouter)
 router.use("/students", studentsRouter)
 router.use("/teachers", teachersRouter)
 
